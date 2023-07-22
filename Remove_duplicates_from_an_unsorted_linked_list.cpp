facbfe549f09f45b89fe629632a553578a84286d
Shivam Raj <shivam1raj5@gmail.com>
2023-07-22 15:03:18 +0530

    _

---
+++ Remove_duplicates_from_an_unsorted_linked_list.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+     // your code goes here
+      Node *temp = head->next;
+        Node *prev = head; 
+        map<int,int> mp;
+        mp[prev->data]++;
+        while(temp != NULL){
+            if(mp.find(temp->data) == mp.end()){
+                mp[temp->data]++;
+                prev->next = temp; 
+                prev = temp;
+                temp = temp -> next;
+            }else{
+                temp = temp->next;
+            }
+            
+        }
+        prev->next = NULL;
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
