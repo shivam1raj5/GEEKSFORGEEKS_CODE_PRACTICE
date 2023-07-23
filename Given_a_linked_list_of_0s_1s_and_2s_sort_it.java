6c58f27737c1b150416af8d3e8fadf8bb2ee9b94
Shivam Raj <shivam1raj5@gmail.com>
2023-07-23 22:21:34 +0530

    _

---
+++ Given_a_linked_list_of_0s_1s_and_2s_sort_it.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Given_a_linked_list_of_0s_1s_and_2s_sort_it
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        Node curr=head;
+        
+        int count[]=new int[3];
+        while(curr!=null){
+            int i=curr.data;
+            count[i]++;
+            curr=curr.next;
+        }
+        Node temp=head;
+        for(int i=0;i<3;i++){
+            while(count[i]>=1 && temp!=null){
+                temp.data=i;
+                temp=temp.next;
+                count[i]--;
+            }
+            
+        }
+      return head;
+    }
+}
+
+
