ae93264e88d4ff28bdb6c2c331978153a0af058e
Shivam Raj <shivam1raj5@gmail.com>
2024-02-25 20:06:11 +0530

    _

---
+++ Reach_a_given_score.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    // Complete this function
+    long long int count(long long int n)
+    {
+    	// Your code here
+    	int ans = 0;
+        for(int i = 0; i<=n/3; i++){
+            int x = n - i*3;
+            if(x%5==0) ans += (1+x/10);
+        }
+        return ans;
+    	
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		Solution obj;
+		cout<<obj.count(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
